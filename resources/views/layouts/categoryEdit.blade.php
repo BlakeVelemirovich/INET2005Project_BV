b36de6b87fdd969d331dfc5401c2f377bdb086fc
Blake Velemirovich <Blake.Velemirovich@gmail.com>
2024-12-02 14:16:23 -0400

    Edit route - View finished

--- resources/views/layouts/categoryEdit.blade.php
+++ resources/views/layouts/categoryEdit.blade.php
@@ -4,9 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Edit Category</title>
+    <link rel="stylesheet" href="{{ asset('css/categories.css') }}">
 </head>
 <body>
+    <form>
+        <legend>
+            <h2>Edit Category: <b style="color: red;"> {{ $category->categoryName }}</b></h2>
+        </legend>
+        <div class="formRow">
+            <label for="categoryName">Category Name:</label>
+            <input
+                type="text"
+                name="categoryName"
+                id="categoryName"
+                placeholder="Enter category name"
+                value="{{ $category->categoryName }}"
+            >
+            @if($errors->has('categoryName'))
+            <div class="error">{{ $errors->first('categoryName') }}</div>
+            @endif
+        </div>
 
+        <input type="submit" value="Submit Form" class="mt-5">
+    </form>
 </body>
 </html>
